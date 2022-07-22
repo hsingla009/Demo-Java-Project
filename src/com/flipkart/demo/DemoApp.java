e90965ff842b95c21fe8206bb688ef5c1248ec6e
Harshit Singla <harshit.singla@BLRETV-QD7RPW7V.local>
2022-07-22 10:07:33 +0530

    first java commit

---
+++ src/com/flipkart/demo/DemoApp.java
@@ -0,0 +1,19 @@
+/**
+ * 
+ */
+package com.flipkart.demo;
+
+/**
+ * @author harshit.singla
+ *
+ */
+public class DemoApp {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("this is my first java project");
+	}
+}
